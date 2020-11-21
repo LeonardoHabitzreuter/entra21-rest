7d07197186873894471d887e6d217eb0e67cdac2
LEONARDO HABITZREUTER <leonardo.habitzreuter@invillia.com>
2020-11-20 21:41:25 -0300

    creating DB using EF

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,6 +1,7 @@
 using Domain.Infra;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,10 +26,11 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // using (var db = new BrasileiraoContext())
-            // {
-                
-            // }
+            using (var db = new BrasileiraoContext())
+            {
+                // Este comando irá criar o banco de dados (quando ele ainda não existir)
+                db.Database.Migrate();
+            }
 
             if (env.IsDevelopment())
             {
