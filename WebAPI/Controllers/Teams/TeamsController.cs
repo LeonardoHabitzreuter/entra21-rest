555115d00325a7eb13a043470e04ccbae1dfa7ab
AndreCampigotto <andre@lojadematec.com.br>
2021-02-04 20:56:24 -0300

    query string sample

--- WebAPI/Controllers/Teams/TeamsController.cs
+++ WebAPI/Controllers/Teams/TeamsController.cs
@@ -51,5 +51,26 @@ namespace WebAPI.Controllers.Teams
             
             return Ok(response.Id);
         }
+
+        [HttpGet]
+        public IActionResult Get(string name)
+        {
+            var teams = new List<Team>
+            {
+                new Team("Atletico Paranaense", new []{"Rony", "Madson", "Marcelo Cirino", "Léo Pereira", "Zé Evaldo"}),
+                new Team("Atletico Mineiro", new []{"Hulk", "Guga", "Réver", "Victor", "Igor Rabelo"}),
+                new Team("Parmera", new []{"Luix Adriano", "Breno", "Dudu", "Zé Rafael", "Marcos Rocha"}),
+                new Team("Framengo", new []{"Coringa", "Neneca", "Gabigol", "Everton Miteiro", "Carrascaeta"})
+            };
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return Ok(teams);
+            }
+
+            var transformedName = name.ToLower().Trim();
+            var filteredTeams = teams.Where(x => x.Name.ToLower().Contains(transformedName));
+            return Ok(filteredTeams);
+        }
     }
 }
